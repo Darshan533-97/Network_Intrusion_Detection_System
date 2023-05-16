e7430264d07e2b18404e5afb0ac196a0542bcb67
Darshan533-97 <124558119+Darshan533-97@users.noreply.github.com>
2023-05-16 10:27:08 -0700

    Update datapath.v

--- src/datapath.v
+++ src/datapath.v
@@ -1,7 +1,7 @@
 `timescale 1ns / 1ps
 //////////////////////////////////////////////////////////////////////////////////
 // Company: 
-// Engineer: 
+// Engineer: Darshan Dyamavvanahalli Rudreshi
 // 
 // Create Date:    22:12:06 02/17/2023 
 // Design Name: 
