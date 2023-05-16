f56c707e6241ef1b6a97535fe422ab4595c92424
Darshan533-97 <124558119+Darshan533-97@users.noreply.github.com>
2023-05-16 10:26:17 -0700

    Update comparator.v

--- src/comparator.v
+++ src/comparator.v
@@ -1,3 +1,23 @@
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: Darshan Dyamavvanahalli Rudreshi
+// 
+// Create Date:    21:49:16 02/14/2023 
+// Design Name: 
+// Module Name:    ALU 
+// Project Name: 
+// Target Devices: 
+// Tool versions: 
+// Description: 
+//
+// Dependencies: 
+//
+// Revision: 
+// Revision 0.01 - File Created
+// Additional Comments: 
+//
+//////////////////////////////////////////////////////////////////////////////////
+
 module comparator( input [55:0] in_data,
                     input [63:0] bloom_array,
                     output match);
